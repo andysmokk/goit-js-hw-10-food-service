42483b95502c937d536f7c2a6710e83c7f49b218
Andrii Konovalov <78738364+andysmokk@users.noreply.github.com>
2021-07-31 17:09:59 +0200

    add card markup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './sass/main.scss';
+import menuCardTpl from './templates/menu-card.hbs';
+import menu from './menu.json';
+
+console.log(menuCardTpl(menu[2]));
+
+const menuRef = document.querySelector('.js-menu');
+
+const cardMarkup = createCardMarkup(menu);
+menuRef.insertAdjacentHTML('beforeend', cardMarkup);
+
+function createCardMarkup(items) {
+  return items.map(menuCardTpl).join('');
+}
