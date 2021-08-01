37c25063a61bc2b5ef2b73889f66b6cad9a9ccfe
Andrii Konovalov <78738364+andysmokk@users.noreply.github.com>
2021-08-01 22:43:14 +0200

    add hw10

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
 import './sass/main.scss';
-import menuCardTpl from './templates/menu-card.hbs';
+import menuCardsTpl from './templates/menu-cards.hbs';
 import menu from './menu.json';
 
-console.log(menuCardTpl(menu[2]));
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
 const menuRef = document.querySelector('.js-menu');
+const checkboxRef = document.querySelector('#theme-switch-toggle');
 
 const cardMarkup = createCardMarkup(menu);
 menuRef.insertAdjacentHTML('beforeend', cardMarkup);
 
 function createCardMarkup(items) {
-  return items.map(menuCardTpl).join('');
+  return menuCardsTpl(items);
 }
+
+checkboxRef.addEventListener('change', onChangeTheme);
+
+function onChangeTheme() {
+  if (checkboxRef.checked) {
+    document.body.classList.add(Theme.DARK);
+    document.body.classList.remove(Theme.LIGHT);
+    localStorage.setItem('Theme', Theme.DARK);
+  } else {
+    document.body.classList.add(Theme.LIGHT);
+    document.body.classList.remove(Theme.DARK);
+    localStorage.setItem('Theme', Theme.LIGHT);
+  }
+}
+
+function saveTheme() {
+  const currentTheme = localStorage.getItem('Theme');
+  if (currentTheme === Theme.DARK) {
+    document.body.classList.add(currentTheme);
+    checkboxRef.checked = true;
+  }
+}
+
+saveTheme();
